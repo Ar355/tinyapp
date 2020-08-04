ad56adfddd93c5214633228b63c186cd14bb6014
Ar355 <zeneli.arian@gmail.com>
2020-08-04 19:55:19 +0000

    commitign delet butt

--- express-server.js
+++ express-server.js
@@ -12,7 +12,7 @@ const generateRandomString = function() {
   return Math.random().toString(36).substring(2,8);
 };
 
-app.get("/urls", (req, res) => {
+app.get("/", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
@@ -37,6 +37,12 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+
 app.post("/urls", (req, res) => {
   
   const shortURL = generateRandomString();
@@ -44,13 +50,10 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
-app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
-});
-
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.post("/urls/:shortURL/delete", (req, res) => {
+  
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
 });
 
 app.listen(PORT, () => {
