7d584fcd7e222767be60dfa8cd751daf67ddce81
Ar355 <zeneli.arian@gmail.com>
2020-08-04 01:55:47 +0000

    add new shorturl route

--- express-server.js
+++ express-server.js
@@ -4,7 +4,10 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
-
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -14,6 +17,11 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: "http://www.lighthouselabs.ca" };
+  res.render("urls_show", templateVars);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
